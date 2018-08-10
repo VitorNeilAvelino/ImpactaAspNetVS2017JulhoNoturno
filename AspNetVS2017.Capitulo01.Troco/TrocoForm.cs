77ca9adeb1200a67d215fd4b420ffaca650ee61b
Vitor Neil Avelino <avelino.vitor@gmail.com>
2018-08-10 20:49:12 -0300

    CultureInfo

--- AspNetVS2017.Capitulo01.Troco/TrocoForm.cs
+++ AspNetVS2017.Capitulo01.Troco/TrocoForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +18,15 @@ namespace AspNetVS2017.Capitulo01.Troco
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void calcularButton_Click(object sender, EventArgs e)
         {
+            var valorCompra = Convert.ToDecimal(valorCompraTextBox.Text);
+            var valorPago = Convert.ToDecimal(valorPagoTextBox.Text);
 
+            var troco = valorPago - valorCompra;
+
+            //trocoTextBox.Text = Convert.ToString(troco);
+            trocoTextBox.Text = troco.ToString("C", new CultureInfo("en-US"));
         }
     }
 }
