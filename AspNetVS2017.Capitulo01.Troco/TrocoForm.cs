968eba2f72757dd11b2b1431dcf02c58b5ac6d66
Vitor Neil Avelino <avelino.vitor@gmail.com>
2018-08-14 22:39:11 -0300

    Coleções

--- AspNetVS2017.Capitulo01.Troco/TrocoForm.cs
+++ AspNetVS2017.Capitulo01.Troco/TrocoForm.cs
@@ -29,33 +29,49 @@ namespace AspNetVS2017.Capitulo01.Troco
             trocoTextBox.Text = troco.ToString("C", new CultureInfo("en-US"));
 
             //ToDo: refatorar para usar vetor e for.
+            var moedas = new decimal[] { 1, 0.5m, 0.25m, 0.10m, 0.05m, 0.01m };
+            //var indice = 0;
 
-            //var moedas1 = Convert.ToInt32(troco / 1);
-            var moedas1 = (int)troco;
-            //troco = troco - moedas1;
-            troco = troco % 1;
+            //foreach (var moeda in moedas)
+            //{
+            //    //moedasListView.Items[indice++].Text = ((int)(troco / moeda)).ToString();
+            //    //moedasListView.Items[moedas.ToList().IndexOf(moeda)].Text = 
+            //    //    ((int)(troco / moeda)).ToString();
 
-            var moedas050 = (int)(troco / 0.5M);
-            troco %= 0.5m;
+            //}
 
-            var moedas025 = (int)(troco / 0.25M);
-            troco %= 0.25m;
+            for (int i = 0; i < moedas.Length; i++)
+            {
+                moedasListView.Items[i].Text = ((int)(troco / moedas[i])).ToString();
+                troco %= moedas[i];
+            }
 
-            var moedas010 = (int)(troco / 0.1M);
-            troco %= 0.1m;
+            ////var moedas1 = Convert.ToInt32(troco / 1);
+            //var moedas1 = (int)troco;
+            ////troco = troco - moedas1;
+            //troco = troco % 1;
 
-            var moedas005 = (int)(troco / 0.05M);
-            troco %= 0.05m;
+            //var moedas050 = (int)(troco / 0.5M);
+            //troco %= 0.5m;
 
-            var moedas001 = (int)(troco / 0.01M);
-            troco %= 0.01m;
+            //var moedas025 = (int)(troco / 0.25M);
+            //troco %= 0.25m;
 
-            moedasListView.Items[0].Text = moedas1.ToString();
-            moedasListView.Items[1].Text = moedas050.ToString();
-            moedasListView.Items[2].Text = moedas025.ToString();
-            moedasListView.Items[3].Text = moedas010.ToString();
-            moedasListView.Items[4].Text = moedas005.ToString();
-            moedasListView.Items[5].Text = moedas001.ToString();            
+            //var moedas010 = (int)(troco / 0.1M);
+            //troco %= 0.1m;
+
+            //var moedas005 = (int)(troco / 0.05M);
+            //troco %= 0.05m;
+
+            //var moedas001 = (int)(troco / 0.01M);
+            //troco %= 0.01m;
+
+            //moedasListView.Items[0].Text = moedas1.ToString();
+            //moedasListView.Items[1].Text = moedas050.ToString();
+            //moedasListView.Items[2].Text = moedas025.ToString();
+            //moedasListView.Items[3].Text = moedas010.ToString();
+            //moedasListView.Items[4].Text = moedas005.ToString();
+            //moedasListView.Items[5].Text = moedas001.ToString();            
         }
     }
 }
--- AspNetVS2017.Capitulo01.VetoresColecoes.Testes/VetoresTeste.cs
+++ AspNetVS2017.Capitulo01.VetoresColecoes.Testes/VetoresTeste.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace AspNetVS2017.Capitulo01.VetoresColecoes.Testes
 {
@@ -20,7 +21,7 @@ namespace AspNetVS2017.Capitulo01.VetoresColecoes.Testes
 
             var decimais = new decimal[] { 0.3m, 0.2m, 7m };
 
-            decimal[] outroVetor = { 0.5m, 8.6m, 12};
+            decimal[] outroVetor = { 0.5m, 8.6m, 12 };
 
             foreach (var @decimal in decimais)
             {
@@ -28,6 +29,76 @@ namespace AspNetVS2017.Capitulo01.VetoresColecoes.Testes
             }
 
             Console.WriteLine("Tamanho do vetor:" + decimais.Length);
+        }
+
+        [TestMethod]
+        public void RedimensionamentoTeste()
+        {
+            var decimais = new decimal[] { 3.1m, 9.3m, 10 };
+
+            //decimais[4] = 1.8m;
+
+            Array.Resize(ref decimais, 5);
+
+            decimais[4] = 1.8m;
+        }
+
+        [TestMethod]
+        public void OrdenacaoTeste()
+        {
+            var decimais = new decimal[] { 3.1m, 9.3m, 1.3m };
+
+            Array.Sort(decimais);
+            //Linq
+
+            Assert.AreEqual(decimais[0], 1.3m);
+        }
+
+        [TestMethod]
+        public void ParamsTeste()
+        {
+            var decimais = new decimal[] { 3.1m, 9.3m, 1.3m };
+
+            Console.WriteLine(Media(decimais));
+
+            Console.WriteLine(Media(3, 8.9m, 0.87m, 20));
+        }
+
+        [TestMethod]
+        public void TodaStringEhUmVetorTeste()
+        {
+            //StringBuilder
+            var nome = "Hejlsberg";
+
+            Assert.AreEqual(nome[0], 'H');
+
+            foreach (var @char in nome)
+            {
+                Console.Write(@char);
+            }
+        }
+
+        private decimal Media(decimal num1, decimal num2)
+        {
+            //decimal media;
+            //media = (num1 + num2) / 2;
+            //return media;
+
+            return (num1 + num2) / 2;
+        }
+
+        private decimal Media(params decimal[] decimais)
+        {
+            //return decimais.Average();
+
+            decimal soma = 0;
+            //var cont = decimais.Length;
+            foreach (var numero in decimais)
+            {
+                soma += numero;
+            }
+
+            return soma / decimais.Length;
         }
     }
 }
