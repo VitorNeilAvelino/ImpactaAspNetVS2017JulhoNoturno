cccff56d388f430962a09de5a90d212481bc0960
Vitor Neil Avelino <avelino.vitor@gmail.com>
2018-08-27 22:48:51 -0300

    Destinos - MVC

---
+++ ViagensOnline.Mvc/Areas/Admin/Controllers/DestinosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ViagensOnline.Dominio;
+using ViagensOnline.Repositorios.SqlServer;
+
+namespace ViagensOnline.Mvc.Areas.Admin.Controllers
+{
+    public class DestinosController : Controller
+    {
+        private ViagensOnlineDbContext db = new ViagensOnlineDbContext();
+
+        // GET: Admin/Destinos
+        public ActionResult Index()
+        {
+            return View(db.Destinos.ToList());
+        }
+
+        // GET: Admin/Destinos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Destino destino = db.Destinos.Find(id);
+            if (destino == null)
+            {
+                return HttpNotFound();
+            }
+            return View(destino);
+        }
+
+        // GET: Admin/Destinos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Destinos/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Pais,Cidade,NomeImagem")] Destino destino)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Destinos.Add(destino);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(destino);
+        }
+
+        // GET: Admin/Destinos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Destino destino = db.Destinos.Find(id);
+            if (destino == null)
+            {
+                return HttpNotFound();
+            }
+            return View(destino);
+        }
+
+        // POST: Admin/Destinos/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Pais,Cidade,NomeImagem")] Destino destino)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(destino).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(destino);
+        }
+
+        // GET: Admin/Destinos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Destino destino = db.Destinos.Find(id);
+            if (destino == null)
+            {
+                return HttpNotFound();
+            }
+            return View(destino);
+        }
+
+        // POST: Admin/Destinos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Destino destino = db.Destinos.Find(id);
+            db.Destinos.Remove(destino);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ViagensOnline.Repositorios.SqlServer/ViagensOnlineDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ViagensOnline.Dominio;
+
+namespace ViagensOnline.Repositorios.SqlServer
+{
+    // Hibernate - Java
+    public class ViagensOnlineDbContext : DbContext
+    {
+        public ViagensOnlineDbContext() : base("viagensOnlineConnectionString")
+        {
+
+        }
+
+        public DbSet<Destino> Destinos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
